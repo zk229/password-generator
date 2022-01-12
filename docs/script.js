a16fdf157d34b1f6cc5f775a10ff5e4e640b1334
zk229 <zkirsche01@gmail.com>
2022-01-11 19:19:34 -0500

    implement an object to better organize user input

--- docs/script.js
+++ docs/script.js
@@ -1,53 +1,63 @@
 // Generate a password
-var length = 0;
-var lowercase = "";
-var uppercase = "";
-var numeric = "";
-var special = "";
+var input = {
+  length: 0,
+  lowercase: "",
+  uppercase: "",
+  numeric: "",
+  special: "",
+  valid: false
+}
+var charSet = [];
 
 function getInput() {
   // Continue prompting until a valid input is received
-  while(length < 8 || length > 128) {
-    length = window.prompt("How long do you want your password to be? (8-128)");
+  while(input.length < 8 || input.length > 128) {
+    input.length = window.prompt("How long do you want your password to be? (8-128)");
   }
-  while(lowercase != "Y" && lowercase != "N") {
-    lowercase = window.prompt("Do you want to include lowercase characters? (Y/N)");
+  while(input.lowercase != "Y" && input.lowercase != "N") {
+    input.lowercase = window.prompt("Do you want to include lowercase characters? (Y/N)");
   }
-  while(uppercase != "Y" && uppercase != "N") {
-    uppercase = window.prompt("Do you want to include uppercase characters? (Y/N)");
+  while(input.uppercase != "Y" && input.uppercase != "N") {
+    input.uppercase = window.prompt("Do you want to include uppercase characters? (Y/N)");
   }
-  while(numeric != "Y" && numeric != "N") {
-    numeric = window.prompt("Do you want to include numeric characters? (Y/N)");
+  while(input.numeric != "Y" && input.numeric != "N") {
+    input.numeric = window.prompt("Do you want to include numeric characters? (Y/N)");
   }
-  while(special != "Y" && special != "N") {
-    special = window.prompt("Do you want to include special characters? (Y/N)");
+  while(input.special != "Y" && input.special != "N") {
+    input.special = window.prompt("Do you want to include special characters? (Y/N)");
   }
+  input.valid = true;
   // Make sure the user has selected "Y" at least once - otherwise, restart
-  if(lowercase == "N" && uppercase == "N" && numeric == "N" && special == "N") {
+  if(input.lowercase == "N" && input.uppercase == "N" && input.numeric == "N" && input.special == "N") {
     window.alert("Error! You didn't include any characters in your set.  Try again.");
-    getInput();
+    input.valid = false;
+    input.lowercase = "";
+    input.uppercase = "";
+    input.numeric = "";
+    input.special = "";
   }
+  return;
 }
 
 function generateCharSet() {
   charSet = [];
   // Automate generation of valid character set using ASCII values
-  if(lowercase == "Y") {
+  if(input.lowercase == "Y") {
     for(var i = 97; i <= 122; i++) {
       charSet.push(String.fromCharCode(i));
     }
   }
-  if(uppercase == "Y") {
+  if(input.uppercase == "Y") {
     for(var i = 65; i <= 90; i++) {
       charSet.push(String.fromCharCode(i));
     }
   }
-  if(numeric == "Y") {
+  if(input.numeric == "Y") {
     for(var i = 48; i <= 57; i++) {
       charSet.push(String.fromCharCode(i));
     }
   }
-  if(special == "Y") {
+  if(input.special == "Y") {
     for(var i = 33; i <= 47; i++) {
       charSet.push(String.fromCharCode(i));
     }
@@ -65,11 +75,13 @@ function generateCharSet() {
 }
 
 function generatePassword() {
-  getInput();
+  while(!input.valid) {
+    getInput();
+  }
   generateCharSet();
   // Generate password
   var finalPassword = "";
-  for(var i = 0; i < length; i++) {
+  for(var i = 0; i < input.length; i++) {
     var randValue = Math.floor(Math.random() * charSet.length);
     finalPassword += charSet[randValue];
   }
